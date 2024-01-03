d0614380142d6e5d70e6e108c3df5e16581ee45b
Vladilen Karassev <Vladilen@Karassev.com>
2024-01-03 16:38:19 -0600

    initial

---
+++ app-01-get-web.py
@@ -0,0 +1,10 @@
+from office365.sharepoint.client_context import ClientContext
+from appsecrets import test_client_id, test_client_secret, test_site_url
+
+ctx = ClientContext(test_site_url).with_client_credentials(
+    test_client_id, test_client_secret
+)
+target_web = ctx.web.get().execute_query()
+print(target_web.url)
+print(target_web.created)
+
